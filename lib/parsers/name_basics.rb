f45b2897188bc622c3b2332c75b8f96f1342c144
Basavanagowda Kanur <basavanagowda@gmail.com>
2021-12-10 00:09:29 +0530

    Include writings & directings foreign keys in name parser

--- lib/parsers/name_basics.rb
+++ lib/parsers/name_basics.rb
@@ -11,6 +11,8 @@ module Parsers
     }.freeze
 
     def before_call
+      ActiveRecord::Base.connection.remove_foreign_key :writings, :artists
+      ActiveRecord::Base.connection.remove_foreign_key :directings, :artists
       ActiveRecord::Base.connection.remove_index :artists, :imdb_id
     end
 
@@ -20,6 +22,12 @@ module Parsers
 
     def after_call
       ActiveRecord::Base.connection.add_index :artists, :imdb_id, unique: true, if_not_exists: true
+      ActiveRecord::Base.connection.add_foreign_key :directings,
+        :artists,
+        column: :director_imdb_id,
+        primary_key: :imdb_id
+
+      ActiveRecord::Base.connection.add_foreign_key :writings, :artists, column: :writer_imdb_id, primary_key: :imdb_id
     end
 
     def name
