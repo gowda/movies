fc16c5406a364e58426fa6be41757e01c909c7c1
Basavanagowda Kanur <basavanagowda@gmail.com>
2021-12-10 01:01:29 +0530

    Add association for title & episodes

--- app/models/title.rb
+++ app/models/title.rb
@@ -6,4 +6,14 @@ class Title < ApplicationRecord
   has_many :directors, through: :directings
   has_many :writings, dependent: nil, foreign_key: :title_imdb_id, primary_key: :imdb_id, inverse_of: :title
   has_many :writers, through: :writings
+  has_many :title_episodes, dependent: nil, foreign_key: :title_imdb_id, primary_key: :imdb_id, inverse_of: false
+  has_many :episodes, through: :title_episodes, source: :episode
+
+  has_one :parent_title,
+    class_name: 'TitleEpisode',
+    dependent: nil,
+    foreign_key: :episode_imdb_id,
+    primary_key: :imdb_id,
+    inverse_of: false
+  has_one :parent, class_name: 'Title', through: :parent_title, source: :title
 end
--- app/models/title_episode.rb
+++ app/models/title_episode.rb
@@ -1,2 +1,10 @@
+# frozen_string_literal: true
+
 class TitleEpisode < ApplicationRecord
+  belongs_to :title, foreign_key: :title_imdb_id, primary_key: :imdb_id, inverse_of: :title_episodes
+  belongs_to :episode,
+    class_name: 'Title',
+    foreign_key: :episode_imdb_id,
+    primary_key: :imdb_id,
+    inverse_of: :parent_title
 end
