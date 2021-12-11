50a3d9d41beeff27f4717e89964a4cc5dce57a8c
Basavanagowda Kanur <basavanagowda@gmail.com>
2021-12-11 11:57:11 +0530

    Add seed data for IMDb datasets

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,34 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+[
+  {
+    name: 'title.basics',
+    display_name: 'Titles'
+  },
+  {
+    name: 'name.basics',
+    display_name: 'Artists'
+  },
+  {
+    name: 'title.akas',
+    display_name: 'Alternative names'
+  },
+  {
+    name: 'title.crew',
+    display_name: 'Directors & writers'
+  },
+  {
+    name: 'title.episode',
+    display_name: 'Episodes'
+  },
+  {
+    name: 'title.principals',
+    display_name: 'Principal cast & crew'
+  },
+  {
+    name: 'title.ratings',
+    display_name: 'IMDb Ratings'
+  }
+].each do |attrs|
+  Dataset.create!(attrs)
+end
