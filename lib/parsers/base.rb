a5166252b1eaf8627f7eaee1674bc739a251884e
Basavanagowda Kanur <basavanagowda@gmail.com>
2021-12-11 12:17:48 +0530

    Use Dataset in parser

--- lib/parsers/base.rb
+++ lib/parsers/base.rb
@@ -4,13 +4,7 @@ require 'csv'
 
 module Parsers
   class Base
-    attr_accessor :index
-
     SLICE_SIZE = 10_000
-
-    def initialize
-      @index = 0
-    end
 
     def call
       before_call
@@ -18,21 +12,22 @@ module Parsers
       CSV.foreach(path, options).each_slice(SLICE_SIZE) do |rows|
         insert_rows!(rows)
 
-        self.index += rows.length
+        dataset.increment!(:completed, rows.length, touch: true)
         print_progress
       end
 
       after_call
+      dataset.update(fetched: true)
       print_completion
     end
 
     def print_progress
-      printf "\rCreating#{dots.next.ljust(5, ' ')} #{index.to_s.rjust(10, ' ')}/#{count}"
+      printf "\rCreating#{dots.next.ljust(5, ' ')} #{completed.to_s.rjust(10, ' ')}/#{total}"
     end
 
     def print_completion
       printf "\r#{' ' * 80}"
-      printf "\rCreated #{index} records\n"
+      printf "\rCreated #{completed} records\n"
     end
 
     def before_call
@@ -43,14 +38,6 @@ module Parsers
 
     def name
       raise NotImplemented, 'do not know which file to read from'
-    end
-
-    def count
-      @count ||= `wc -l #{path}`.split.first.to_i - 1
-    end
-
-    def path
-      Rails.root.join("tmp/#{name}.tsv")
     end
 
     def options
@@ -74,6 +61,14 @@ module Parsers
     def exceptions
       []
     end
+
+    def dataset
+      @dataset ||= Dataset.find_by(name: name)
+    end
+
+    delegate :completed, to: :dataset
+    delegate :total, to: :dataset
+    delegate :path, to: :dataset
 
     def dots
       @dots ||= (1..4).map { |i| '.' * i }.cycle
