3377ca3106a26d310e04177cee02a43d4241afd5
Basavanagowda Kanur <basavanagowda@gmail.com>
2021-12-14 11:54:59 +0530

    Add after_save callback to update parent imdb rating

--- app/models/imdb_rating.rb
+++ app/models/imdb_rating.rb
@@ -2,4 +2,12 @@
 
 class IMDbRating < ApplicationRecord
   belongs_to :title, foreign_key: :title_imdb_id, primary_key: :imdb_id, inverse_of: :rating
+
+  after_save :update_parent_rating
+
+  private
+
+  def update_parent_rating
+    title.update!(imdb_rating: average_rating)
+  end
 end
